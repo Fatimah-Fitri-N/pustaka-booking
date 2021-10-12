18568dc72707d9f0ddf053da6f5bd5c38bc25035
Fatimah-Fitri-N <fatimahfitri313@gmail.com>
2021-10-12 23:08:37 +0700

    membuat template web sederhana

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+  <h1><?php echo $judul ?></h1>
+  <h4>Nama</h4>
+  <ul type="disc">
+    <li>Nama Depan : Fatimah</li>
+    <li>Nama Belakang : Fitri</li>
+  </ul>
+  <br>
+  <h4>Alamat</h4>
+  <ul type="none">
+    <li> Jalan Wates Km.10 </li>
+  </ul>
+
+  <h4>Tempat Lahir</h4>
+  <ul type="none">
+    <li>Bantul</li>
+  </ul>
+
+  <h4>Olahraga Favorit</h4>
+  <ul type="square">
+    <li>Bulu Tangkis</li>
+    <li>Adventure</li>
+  </ul>
+
+</section>
