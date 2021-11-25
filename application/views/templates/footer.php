223277046efd0e55f6438e90711bb90bc3e8aaf8
Fatimah-Fitri-N <fatimahfitri313@gmail.com>
2021-11-25 14:07:03 +0700

    pertemuan 10

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -26,7 +26,7 @@ aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Yakin mau keluar?</h5>
-                <button class="close" type="button" datadismiss="modal" aria-label="Close">
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
                 </button>
             </div>
@@ -44,10 +44,10 @@ data-dismiss="modal">Cancel</button>
 <script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="<?= base_url('assets/'); ?>vendor/jqueryeasing/jquery.easing.min.js"></script>
+<script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="<?= base_url('assets/'); ?>js/sb-admin2.min.js"></script>
+<script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
 
 <script>
     $('.custom-file-input').on('change', function() {
